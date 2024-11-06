032840cc487278808ab9e0acb9023f666f3c6a57
viettq3 <viettq3@rikkeisoft.com>
2024-11-06 14:53:47 +0700

    websocket

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -7,8 +7,8 @@ export default function SignUp() {
   const handleSignUp = async () => {
     try {
       const { data } = await axiosInstance.post('/auth/sign-up', {
-         username: 'johnsdssoes21',
-         email: 'johndosssses21@gmail.com',
+         username: 'johnssdssoes21',
+         email: 'johndosssssses21@gmail.com',
          password: '123456',
          roles: ['3']
       })
