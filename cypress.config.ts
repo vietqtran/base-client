64fada4838321545ce82f79b26012174952c2b62
vietqtran <tranquocviet1303@gmail.com>
2024-11-13 17:20:43 +0700

    cypress

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+   e2e: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      setupNodeEvents(on, config) {
+         // implement node event listeners here
+      }
+   },
+
+   component: {
+      devServer: {
+         framework: 'next',
+         bundler: 'webpack'
+      }
+   }
+})
